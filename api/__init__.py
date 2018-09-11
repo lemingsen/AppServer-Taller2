1827a4b7d6c7e65d67468efc306a5650114d8294
lucas <nesgnimmehl@gmail.com>
2018-09-11 01:15:02 -0300

    Lint corrections

--- api/__init__.py
+++ api/__init__.py
@@ -1,9 +1,9 @@
+"""api init"""
 from flask import Blueprint
 from flask_restplus import Api
 from .hello import api as hello
 
 blueprint = Blueprint('api', __name__)
-api = Api(blueprint, title='Shared Server API',version='1.0',description='A description')
+api = Api(blueprint, title='Shared Server API',
+          version='1.0', description='A description')
 api.add_namespace(hello)
-
-
--- api/hello.py
+++ api/hello.py
@@ -1,8 +1,12 @@
+"""Hello World"""
 from flask_restplus import Namespace, Resource
 
 api = Namespace('hello', description='Hello World namespace')
 
+
 @api.route('/')
 class HelloWorld(Resource):
+    """hello route"""
     def get(self):
+        """hello GET"""
         return {'hello': 'world'}
