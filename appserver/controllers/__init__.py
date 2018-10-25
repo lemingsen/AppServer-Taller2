1008a85f5bdacf8e4373859e730f39a8e44c1fa5
lucas <nesgnimmehl@gmail.com>
2018-10-25 18:09:44 -0300

    name refactor root_controller

--- appserver/controllers/__init__.py
+++ appserver/controllers/__init__.py
@@ -2,4 +2,4 @@
 from flask import Blueprint
 
 api = Blueprint('controllers', __name__)
-from appserver.controllers import root, users_controller, products_controller, handlers
+from appserver.controllers import root_controller, users_controller, products_controller, handlers
