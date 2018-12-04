a1660713e4d3c3a686a2db9df23163cbd8ef3249
lucas <nesgnimmehl@gmail.com>
2018-12-04 11:55:44 -0300

    - Removed payment methods management from app server

--- appserver/controllers/payment_method_controllers.py
+++ appserver/controllers/payment_method_controllers.py
@@ -1,28 +1,7 @@
 """Endpoints relacionados a formas de pago"""
-from flask import jsonify, request, abort
+from flask import jsonify
 from appserver.controllers import api_bp
-from appserver.services.payment_method_services import PaymentsService
 from appserver.services.shared_server_services import SharedServer
-
-
-@api_bp.route('/payments', methods=['POST'])
-def add_payment_method():
-    """Agrega un método de pago"""
-    if not request.is_json:
-        abort(400)
-    payment_dict = request.get_json()
-    payment_method = PaymentsService.add_payment_method(payment_dict)
-    return jsonify(payment_method), 200
-
-
-@api_bp.route('/payments/<string:payment_id>', methods=['PUT'])
-def modify_payment_method(payment_id):
-    """Modifica un metodo de pago"""
-    if not request.is_json:
-        abort(400)
-    payment_dict = request.get_json()
-    PaymentsService.modify_payment_method(payment_id, payment_dict)
-    return jsonify(result='success'), 200
 
 
 @api_bp.route('/payments', methods=['GET'])
@@ -33,15 +12,4 @@ def get_payment_methods():
     return jsonify(payment_methods), 200
 
 
-@api_bp.route('/payments/<string:payment_id>', methods=['GET'])
-def get_payment_method(payment_id):
-    """Obtiene un método de pago"""
-    payment_method = PaymentsService.get_payment_method(payment_id)
-    return jsonify(payment_method), 200
 
-
-@api_bp.route('/payments/<string:payment_id>', methods=['DELETE'])
-def delete_payment_method(payment_id):
-    """Borra un método de pago"""
-    PaymentsService.delete_payment_method(payment_id)
-    return jsonify(result='success'), 200
