d195ff59cb362ef920ec5a94ea05f615839027f9
lucas <nesgnimmehl@gmail.com>
2018-12-13 03:57:25 -0500

    FIX: changed location longitude to x and location latitude to y so mongo stores longitude first

--- appserver/models/location.py
+++ appserver/models/location.py
@@ -6,17 +6,17 @@ from marshmallow import Schema, fields, post_load
 
 class Location:
     """Location"""
-    def __init__(self, longitude, latitude):
-        self.longitude = longitude
-        self.latitude = latitude
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
     def distance_to(self, other_location):
         """Returns distance in kilometers between two points"""
         earth_radius = 6373.0
         long2 = radians(other_location.longitude)
         lat2 = radians(other_location.latitude)
-        long1 = radians(self.longitude)
-        lat1 = radians(self.latitude)
+        long1 = radians(self.x)
+        lat1 = radians(self.y)
         delta_longitude = long2 - long1
         delta_latitude = lat2 - lat1
         a = (sin(delta_latitude / 2)) ** 2 + cos(lat1) * \
@@ -28,16 +28,16 @@ class Location:
 
 class LocationSchema(Schema):
     """Location marshmallow schema"""
-    longitude = fields.Float(required=True)
-    latitude = fields.Float(required=True)
+    x = fields.Float(required=True)
+    y = fields.Float(required=True)
 
     @post_load
     def make_location(self, data):
         """Deserializes data into a Location object"""
-        return Location(data['longitude'], data['latitude'])
+        return Location(data['x'], data['y'])
 
 
 class LocationModifySchema(Schema):
     """LocationModifySchema"""
-    longitude = fields.Float(required=True)
-    latitude = fields.Float(required=True)
+    x = fields.Float(required=True)
+    y = fields.Float(required=True)
